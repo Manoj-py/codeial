655ba9c8f669bc774acb823a80d61e25d8882f5d
Manoj-py <manojgowda.vr@gmail.com>
2023-07-30 18:52:26 +0530

    branching out for manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,49 @@
+const User = require("../models/users");
+
 module.exports.profile = function (req, res) {
   return res.render("userProfile", {
     title: "Profile",
   });
+};
+
+module.exports.signUp = function (req, res) {
+  return res.render("user_signup", {
+    title: "Codeial | Sing Up",
+  });
+};
+
+module.exports.signIn = function (req, res) {
+  return res.render("user_signin", {
+    title: "Codeial | Sing In",
+  });
+};
+
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email }).then(function (err, user) {
+    if (err) {
+      console.log("User Already present");
+      return res.redirect("/users/sign-in");
+    }
+
+    if (!user) {
+      User.create(req.body).then(function (err, user) {
+        if (err) {
+          console.log("error in creating user");
+          return res.redirect("back");
+        }
+
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+module.exports.createSession = {
+  //TODO Later
 };
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 // importing routes
 const homeController = require("../controllers/home_controller");
-const postController = require("../controllers/post_controller");
+
 
 router.use("/users", require("./users"));
 
 router.get("/", homeController.home);
-router.get("/post", postController.post);
+
 
 module.exports = router;
